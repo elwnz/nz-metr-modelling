cd8ec4c60845be30f67817d6801092904e6017f8
elwnz <ellywardnz@gmail.com>
2026-05-20 11:04:02 +1200

    Update bank_deposit.py

--- metr/bank_deposit.py
+++ metr/bank_deposit.py
@@ -1,12 +1,12 @@
-def metr_bank_deposit (td, REAL_RETURN, INFLATION):
-     """
-    Simple bank deposit model.
-
-    
-    REAL_RETURN: annual interest rate (e.g. 0.05 for 5%)
-    td: personal tax rate on interest (e.g. 0.33 for 33%)
-    INFLATION: inflation rate (e.g. 0.02 for 2%)
-
-    Returns the final after-tax value of the deposit.
+def metr_bank_deposit(td, REAL_RETURN, INFLATION):
     """
-    return td + ((td* INFLATION) / REAL_RETURN)
+    Simple bank deposit METR model.
+
+    td: personal tax rate on interest (e.g. 0.33)
+    REAL_RETURN: real interest rate (e.g. 0.05)
+    INFLATION: inflation rate (e.g. 0.02)
+
+    Returns the METR for the deposit.
+    """
+    return td + ((td * INFLATION) / REAL_RETURN)
+
